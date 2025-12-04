b4869e6e7204b67e16923b7c97172438adf7bd6a
Yuriy Kolerov <ykolerov@synopsys.com>
2025-12-04 10:54:10 +0000

    buildlib: Add Buildlib tool for ARC-V GNU toolchain
    This tool allows building all toolchain libraries (libgcc,
    libstdc++, Picolibc) off the tree. Check README.md or
    riscv64-snps-elf-buildlib -h for guidance.
    
    Note, that arcgnulib.py library is added. Both TCF wrapper
    and Buildlib depend on it.
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-# TCF Wrapper for GNU Toolchain for ARC-V Targets
+# Scripts for GNU Toolchain for ARC-V Targets
 
-`riscv64-snps-elf-tcf-gcc` tool is a TCF wrapper for GCC for ARC-V targets.
-It's intended to be used with TCF files originally prepared for MetaWare tools.
+This repository contains 2 useful scripts for ARC-V GNU toolchain:
+
+1. `riscv64-snps-elf-tcf-gcc` tool is a TCF wrapper for GCC for ARC-V targets.
+   It's intended to be used with TCF files originally prepared for MetaWare tools.
+2. `riscv64-snps-elf-buildlib` tool allows building Picolibc, `libgcc` and `libstdc++`
+   for a particular set of target and optimization options. Then this configuration
+   may be used for building applications using the TCF wrapper.
 
 ## Installation
 
-The wrapper is tries to find `riscv64-snps-elf-gcc` in 2 places:
+Copy all script to the `bin` directory of a toolchain or add a directory with scripts
+to `PATH`. Note that both scripts try to locate `riscv64-snps-elf-gcc` in 2 places:
 
-1. **Put the script to the directory with `riscv64-snps-elf-gcc` binary.**
+1. **The same directory where scripts reside.**
    
-   If script's directory contains `riscv64-snps-elf-gcc`, then this binary is used.
-2. **Or ensure that `riscv64-snps-elf-gcc` is in your `PATH`.**
+   If scripts' directory contains `riscv64-snps-elf-gcc`, then this binary is used.
+2. **The directory in `PATH`.**
 
 ## Usage
+
+### TCF Wrapper
 
 To pass TCF to the wrapper use `-tcf=...` option. Other options are passed
 directly to GCC:
@@ -29,3 +37,48 @@ extra options:
 $ riscv64-snps-elf-tcf-gcc -march=rv32i -mabi=ilp32 -specs=semihost.specs -specs=arcv.specs -T arcv.ld main.c -o main.elf
 riscv64-snps-elf-gcc -march=rv32i -mabi=ilp32 -specs=semihost.specs -specs=arcv.specs -T arcv.ld main.c -o main.elf
 ```
+
+Use `-tcf-help` option to print out information about all available wrapper-specific
+options.
+
+### Buildlib
+
+Buildlib tool allows building all toolchain libraries for a particular set
+of target and optimization options.
+
+Build libraries for `rhx100_base.tcf` template:
+
+```plain
+$ riscv64-snps-elf-buildlib --output rhx100_base --tcf rhx100_base.tcf --nproc 4
+INFO: Found GCC in PATH: /SCRATCH/ykolerov/tools/gcc-arcv-elf-picolibc-latest/bin/riscv64-snps-elf-gcc
+INFO: Final target options:
+-march=rv32i_zicsr_zifencei_zihintpause_a_zca_m_zcb_zcmp_zba_zbb_zbs_zicond_zicbom_zicbop -mabi=ilp32 -mtune=arc-v-rhx-100-series -mcmodel=medlow
+INFO: Final cflags:
+-mno-strict-align -g -O2
+INFO: Cloning Picolibc repository.
+INFO: Creating a build directory: /home/ykolerov/workspace/arc-v/gcc-buildlib/custom-build/arcv-tcf-wrapper/rhx100_base/build-picolibc
+INFO: Configuring Picolibc library.
+INFO: Building Picolibc library.
+INFO: Installing Picolibc library.
+INFO: Cloning GCC repository.
+INFO: Downloading GCC dependencies.
+INFO: Creating a build directory: /home/ykolerov/workspace/arc-v/gcc-buildlib/custom-build/arcv-tcf-wrapper/rhx100_base/build-gcc
+INFO: Configuring GCC.
+INFO: Building libgcc and libstdc++.
+INFO: Installing libgcc and libstdc++.
+INFO: Saving buildlib.specs.
+INFO: Finished building and installing libgcc and libstdc++.
+```
+
+Then you can use these libraries for building applications. You can use the same template for consistency:
+
+```plain
+$ riscv64-snps-elf-tcf-gcc -tcf=rhx100_base.tcf -tcf-buildlib=./rhx100_base -specs=picolibc.specs --oslib=semihost --crt0=semihost hello.c -o hello.elf
+riscv64-snps-elf-gcc -mno-strict-align -march=rv32i_zicsr_zifencei_zihintpause_a_zca_m_zcb_zcmp_zba_zbb_zbs_zicond_zicbom_zicbop -mabi=ilp32 -mtune=arc-v-rhx-100-series -Wl,-defsym=txtmem_addr=0x0 -Wl,-defsym=__flash=0x0 -Wl,-defsym=txtmem_len=0x20000 -Wl,-defsym=__flash_size=0x20000 -Wl,-defsym=datamem_addr=0x200000 -Wl,-defsym=__ram=0x200000 -Wl,-defsym=datamem_len=0x8000 -Wl,-defsym=__ram_size=0x8000 -specs=/home/ykolerov/workspace/arc-v/gcc-buildlib/custom-build/arcv-tcf-wrapper/rhx100_base/buildlib.specs -B/home/ykolerov/workspace/arc-v/gcc-buildlib/custom-build/arcv-tcf-wrapper/rhx100_base/lib/gcc -B/home/ykolerov/workspace/arc-v/gcc-buildlib/custom-build/arcv-tcf-wrapper/rhx100_base/riscv64-snps-elf/lib -specs=picolibc.specs --oslib=semihost --crt0=semihost hello.c -o hello.elf
+
+$ nsimdrv -tcf rhx100_base.tcf -p nsim_semihosting=1 -p enable_exceptions=0 hello.elf
+Hello, World!
+```
+
+Use `--help` option to print out information about all available wrapper-specific
+options.
---
+++ arcgnulib.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2026, Synopsys, Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3) Neither the name of the Synopsys, Inc., nor the names of its contributors
+# may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import logging
+import sys
+import xml.etree.ElementTree as ET
+from typing import Optional
+
+class TCF:
+    def __init__(self, filename: str, no_compressed: bool=False):
+        self._filename = filename
+        self._no_compressed = no_compressed
+        self._init_root_node()
+        self._init_compile_options()
+        self._init_link_options()
+
+    def _init_root_node(self):
+        try:
+            self._tree = ET.parse(self._filename)
+            logging.debug('opened TCF: %s', self._filename)
+        except FileNotFoundError:
+            logging.error('File "%s" is not found.', self._filename)
+            sys.exit(1)
+
+        self._root_node = self._tree.getroot()
+
+    def _init_compile_options(self):
+        # Extract compile options
+        options_node = self._root_node.find("./configuration[@name='gcc_compiler']/string")
+
+        if options_node is None:
+            logging.error('gcc_compiler configuration is not found.')
+            sys.exit(1)
+
+        self._compile_options_list = []
+        compile_options_list_raw = [option.strip() for option in options_node.text.split()]
+
+        # Generate -march, -mtune and -mabi
+        self._march = None
+        self._mtune = None
+        self._mabi = None
+        self._mcmodel = 'medlow'
+
+        for option in compile_options_list_raw:
+            if option.startswith('-march='):
+                self._march = option.split('=')[1].lower()
+                if self._no_compressed:
+                    candidates = ['c', 'zca', 'zcb', 'zcf', 'zcd', 'zcmp', 'zcmt']
+                    extensions = self.get_extensions()
+                    extensions = list(filter(lambda x: x not in candidates, extensions))
+                    self._march = '_'.join(extensions)
+                option = '-march=' + self._march
+            elif option.startswith('-mtune='):
+                self._mtune = option.split('=')[1].lower()
+            elif option.startswith('-mabi='):
+                self._mabi = option.split('=')[1].lower()
+            self._compile_options_list.append(option)
+
+        if self._march is None:
+            logging.error('-march is not found in TCF.')
+            sys.exit(1)
+
+        if self._mtune is None:
+            logging.error('-mtune is not found in TCF.')
+            sys.exit(1)
+
+        if self._mabi is None:
+            logging.error('-mabi is not found in TCF.')
+            sys.exit(1)
+
+        # Generate -mcmodel for RV64 targets
+        if 'rv64' in self._march:
+            self._mcmodel = 'medany'
+
+        logging.debug('compile options extracted: %s', str(self._compile_options_list))
+
+    def _init_link_options(self):
+        # Extract ICCM and DCCM configurations
+        self._link_options_list = []
+        nsim_node = self._root_node.find("./configuration[@name='nSIM']/string")
+
+        if nsim_node is None:
+            logging.error('nSIM configuration is not found.')
+            sys.exit(1)
+
+        nsim_options_map = {}
+        for nsim_option in nsim_node.text.split():
+            key, value = nsim_option.strip().split('=', maxsplit=1)
+            nsim_options_map[key] = value
+
+        # Newlib/Picolibc toolchains and nSIM use different symbols
+        # for .text and .data sections:
+        #
+        #     Section          Newlib          Picolibc        nSIM
+        #     .text address    txtmem_addr     __flash         iccm0_base
+        #     .text size       txtmem_len      __flash_size    iccm0_size
+        #     .data address    datamem_addr    __ram           dccm_base
+        #     .data size       datamem_len     __ram_size      dccm_size
+        self._iccm_base = nsim_options_map.get('iccm0_base', None)
+        if self._iccm_base is not None:
+            self._link_options_list.append('-Wl,-defsym=txtmem_addr={}'.format(self._iccm_base))
+            self._link_options_list.append('-Wl,-defsym=__flash={}'.format(self._iccm_base))
+
+        self._iccm_size = nsim_options_map.get('iccm0_size', None)
+        if self._iccm_size is not None:
+            self._link_options_list.append('-Wl,-defsym=txtmem_len={}'.format(self._iccm_size))
+            self._link_options_list.append('-Wl,-defsym=__flash_size={}'.format(self._iccm_size))
+
+        self._dccm_base = nsim_options_map.get('dccm_base', None)
+        if self._dccm_base is not None:
+            self._link_options_list.append('-Wl,-defsym=datamem_addr={}'.format(self._dccm_base))
+            self._link_options_list.append('-Wl,-defsym=__ram={}'.format(self._dccm_base))
+
+        self._dccm_size = nsim_options_map.get('dccm_size', None)
+        if self._dccm_size is not None:
+            self._link_options_list.append('-Wl,-defsym=datamem_len={}'.format(self._dccm_size))
+            self._link_options_list.append('-Wl,-defsym=__ram_size={}'.format(self._dccm_size))
+
+        logging.debug('link options extracted: %s', str(self._link_options_list))
+
+    def get_march(self) -> str:
+        return self._march
+
+    def get_family(self) -> str:
+        for family in 'rv32i', 'rv32e', 'rv64i':
+            if self._march.startswith(family):
+                return family
+
+        raise ValueError('march does not start with a correct family')
+
+    def get_extensions(self) -> list[str]:
+        family = self.get_family()
+        march = self._march[len(family):]
+        extensions = [family]
+
+        for extension in march.split('_'):
+            extension = extension.replace('_', '')
+            if len(extension) == 0:
+                continue
+            if extension[0] in ('z', 'x'):
+                extensions.append(extension)
+            else:
+                extensions.extend(list(extension))
+
+        return extensions
+
+    def get_mabi(self) -> str:
+        return self._mabi
+
+    def get_mtune(self) -> str:
+        return self._mtune
+
+    def get_mcmodel(self) -> str:
+        return self._mcmodel
+    
+    def get_iccm_base(self) -> Optional[str]:
+        return self._iccm_base
+
+    def get_iccm_size(self) -> Optional[str]:
+        return self._iccm_size
+
+    def get_dccm_base(self) -> Optional[str]:
+        return self._dccm_base
+
+    def get_dccm_size(self) -> Optional[str]:
+        return self._dccm_size
+
+    def get_compile_options(self) -> list[str]:
+        return self._compile_options_list.copy()
+
+    def get_link_options(self) -> list[str]:
+        return self._link_options_list.copy()
